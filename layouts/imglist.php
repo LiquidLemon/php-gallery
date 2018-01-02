d6c3f592d65e25924df3b655f2105df0d71221bf
LiquidLemon <liquidlemon@protonmail.com>
2018-01-03 00:34:22 +0100

    Add visual indication for private images

--- layouts/imglist.php
+++ layouts/imglist.php
@@ -1,10 +1,11 @@
 <?php
 
 foreach ($imgs as $img) {
+  $private = $img->public ? '' : '[P] ';
   $thumbPath = "/thumb/{$img->id()}.png";
   $imgPath = "/preview/{$img->id()}.png";
   echo "<div class=\"image\">";
   echo "<a href=\"{$imgPath}\"><img src=\"{$thumbPath}\"></a>";
-  echo "<p class=\"caption\">{$img->title}</p>";
+  echo "<p class=\"caption\">{$private}{$img->title}</p>";
   echo "</div>";
 }
