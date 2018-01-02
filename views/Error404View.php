7b80886733825c94da1c27a300ad741afc9609a6
LiquidLemon <liquidlemon@protonmail.com>
2018-01-02 23:54:25 +0100

    Update 404 error page to use the common layout

--- views/Error404View.php
+++ views/Error404View.php
@@ -1,8 +1,9 @@
 <?php
+require_once 'LayoutView.php';
 
-class Error404View {
-  public function render() {
-    http_response_code(404);
-    include '../layouts/404.php';
+class Error404View extends LayoutView {
+  public function __construct() {
+    parent::__construct('404');
+    $this->code = 404;
   }
 }
