8b9dfd5289e559bfd6aa118fc31a09ad43081530
LiquidLemon <liquidlemon@protonmail.com>
2018-01-03 00:37:45 +0100

    Link images owned by the users to the originals

--- layouts/imglist.php
+++ layouts/imglist.php
@@ -1,9 +1,12 @@
 <?php
 
+$user = currentUser();
 foreach ($imgs as $img) {
   $private = $img->public ? '' : '[P] ';
   $thumbPath = "/thumb/{$img->id()}.png";
-  $imgPath = "/preview/{$img->id()}.png";
+  $imgPath = ($user && $user->name == $img->author)
+    ? "/images/{$img->id()}.png"
+    : "/preview/{$img->id()}.png";
   echo "<div class=\"image\">";
   echo "<a href=\"{$imgPath}\"><img src=\"{$thumbPath}\"></a>";
   echo "<p class=\"caption\">{$private}{$img->title}</p>";
